108f17eebed813c1a5ebf730d05a8053520f6795
abiliocipriano <abilio@gmail.com>
2015-01-17 18:33:47 -0200

    Initial Commit

---
+++ IOS-Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS-Project
+//
+//  Created by Abilio Cipriano on 1/17/15.
+//  Copyright (c) 2015 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS-Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS-Project
+//
+//  Created by Abilio Cipriano on 1/17/15.
+//  Copyright (c) 2015 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
