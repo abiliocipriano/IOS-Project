1b0a0847807bf072536c34f4ab479d288c8140c7
abiliocipriano <abilio@gmail.com>
2015-01-17 18:52:45 -0200

    Added a test class

---
+++ IOS-Project/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  IOS-Project
+//
+//  Created by Abilio Cipriano on 1/17/15.
+//  Copyright (c) 2015 Code Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ IOS-Project/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  IOS-Project
+//
+//  Created by Abilio Cipriano on 1/17/15.
+//  Copyright (c) 2015 Code Coalition. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
